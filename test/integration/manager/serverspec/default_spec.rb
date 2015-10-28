dbd09c64c50a63bb635b5ece60eb3cb122bfd977
Ovais Tariq <ovaistariq@gmail.com>
2015-10-28 12:09:23 -0700

    Add serverspec test

--- test/integration/manager/serverspec/default_spec.rb
+++ test/integration/manager/serverspec/default_spec.rb
@@ -1,12 +1,33 @@
 require 'spec_helper'
 
-describe 'mysql-mha::default' do
+set :backend, :exec
 
-  # Serverspec examples can be found at
-  # http://serverspec.org/resource_types.html
+puts "os: #{os}"
 
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
+describe command('whoami') do
+  its(:stdout) { should match /root/ }
+end
+
+describe 'mha4mysql-node package is installed' do
+  describe package('mha4mysql-node') do
+    it { should be_installed }
   end
+end
 
+describe 'mha4mysql-manager package is installed' do
+  describe package('mha4mysql-manager') do
+    it { should be_installed }
+  end
+end
+
+describe 'python-mha_helper package is installed' do
+  describe package('python-mha_helper') do
+    it { should be_installed }
+  end
+end
+
+describe 'MySQL replication is setup correctly and MHA can reach MySQL and SSH' do
+  describe command("/usr/bin/masterha_check_repl --conf /etc/mha/test_pod.conf") do
+    its(:stdout) { should match /MySQL Replication Health is OK./ }
+  end
 end
